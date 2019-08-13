f7569fc21db2958f31608c8c6e239bb56a9d150e
joao.porto <joaov.aporto@gmail.com>
2019-08-13 12:43:25 -0300

    Login validation done

---
+++ www/db_connection.php
@@ -0,0 +1,19 @@
+<?php
+
+function openConn() {
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "admin";
+    $db = "taskim_db";
+
+    $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or
+        die("Connect failed: %s\n". $conn -> error);
+
+    return $conn;
+}
+
+function closeConn($conn) {
+    $conn -> close();
+}
+
+?>
